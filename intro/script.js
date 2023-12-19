5356ffd13c3795361cab9e22ebfbdde1a9d96ee3
Callan <callan.v@fake.com>
2023-12-19 12:46:25 +1100

    Lunch break. Variable and some functions learned

--- intro/script.js
+++ intro/script.js
@@ -1,1 +1,20 @@
-console.log("Hello world!")
+// console.log("Hello world!")
+
+// const maxTemp = 28
+// console.log(maxTemp)
+// maxTemp = 30
+// console.log(maxTemp)
+
+// var x = "Sarah"
+
+// {
+//     console.log(x)
+//     console.log(42)
+//     let y = 5
+// }
+// console.log(y)
+
+let str = "Hello World!"
+
+// console.log(str.slice(2))
+console.log(str.replaceAll("o", "---"))
