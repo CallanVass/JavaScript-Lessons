a2ca67706d23aea655c6eb09f4b33b907b588a92
Callan <callan.v@fake.com>
2023-12-19 14:34:24 +1100

    Finished intro to JS

--- intro/script.js
+++ intro/script.js
@@ -17,4 +17,45 @@
 let str = "Hello World!"
 
 // console.log(str.slice(2))
-console.log(str.replaceAll("o", "---"))
+// console.log(str.replaceAll("o", "---"))
+// let name = "Callan"
+
+// console.log(`Hello, ${name}!`)
+// let james = `Hello, ${name}!`
+// console.log(james)
+
+// console.log(Math.floor(3 / 2))
+
+// let x = 5
+// console.log(x++) // post increment
+// console.log(++x) // pre increment
+// Increment X
+// x++
+// Also x-- for decrament
+// console.log(x)
+
+// console.log("123" == 123) // type coercion
+// console.log("123" === 123) // type coercion (strict comparison - stops type coercion)
+
+// let x = 5
+
+// console.log(typeof x) // type of data in variable
+
+// person = {
+//     name: "John", // creating an object
+//     age: 26
+// }
+// let key = "name"
+// console.log(person.age) // calling the object
+// console.log(person[key]) // calling the object
+
+
+// x = [1, 2, 3, true, "Hello"]
+
+// console.log(x[x.length-1])
+
+function add(x, y) { // define a function
+    return x + y
+}
+
+console.log(add(10, 45))
