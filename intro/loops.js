6d38aafff521088128716baab27128c4fa65473d
Callan <callan.v@fake.com>
2023-12-20 14:20:43 +1100

    Completed lesson on loops

---
+++ intro/loops.js
@@ -0,0 +1,61 @@
+// Python
+// count = 3
+// While count > 0:
+//    print(count)
+
+
+
+// let count = 3
+
+// while (count > 0) {
+//     console.log(count--)
+// }
+
+// Python
+// for i in range(10):
+//    print(i)
+
+// 3 part for loop
+//  initializer runs once, before the first iteration
+//  condition will be tested before every iteration
+// post-iteration executes after every iteration
+
+// for (initializer; condition; post-iteration) {}
+
+// print out each number to console
+// const numbers1 = [1, 2, 3, 4, 23, 12, 9]
+// for (let i = 0; i < numbers1.length; i++) {
+//     console.log(numbers1[i])
+// }
+
+// // Print as an array
+// const numbers = [1, 2, 5, 21, 44, 37];
+// const resultArray = [];
+
+// for (let i = 0; i < numbers.length; i++) {
+//     resultArray.push(numbers[i]);
+// }
+
+// console.log(resultArray);
+// Fibonacci
+// for (let prev=1, next=1; next <= 1000; tmp=next, next=prev+next, prev=tmp) {
+//     console.log(next)
+// }
+
+
+
+// Iterate through a collection
+const favFoods = ["Pizza", "Pasta", "Tacos"]
+// for (let item of favFoods) {
+//     console.log(item)
+// }
+
+favFoods.forEach((food, index) => {
+    console.log(`${index+1}. ${food}`)
+})
+
+favFoods.forEach(food => {
+    console.log(food)
+})
+
+
