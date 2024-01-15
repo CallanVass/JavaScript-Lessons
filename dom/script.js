750f03a1a3b22d1e8ceed37fe9c456a7b63e5678
Callan <callan.v@fake.com>
2024-01-15 13:06:28 +1100

    Learned how to dynamically change elements/add them via JS script

--- dom/script.js
+++ dom/script.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+// getElementById is the old way of getting elements (before ES6 (2015))
+// const el = document.querySelectorAll('li')
+// console.log(el)
+
+// el.innerHTML = 'Hey <span style="color: red">World!</span>'
+
+// Programatically/Dynamically add an element to the DOM
+const newDiv = document.createElement("div")
+console.log(newDiv)
+// Insert created element into the DOM
+document.body.appendChild(newDiv)
+// OR
+document.body.insertBefore(newDiv, document.querySelector("ul"))
+
+// Add content to div (Add any changes before insertion, though.)
+newDiv.innerHTML = '<h3>Awesome Content!</h3>'
