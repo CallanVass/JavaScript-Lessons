d74e62090b20f28756560e5a1bf8c5b5cfbb9068
Callan <callan.v@fake.com>
2024-01-17 14:44:02 +1100

    Completed lesson on fetching

--- callbacks/script.js
+++ callbacks/script.js
@@ -5,27 +5,73 @@
 // adder(10, 5, result => console.log(result))
 // console.log("Done")
 function getJoke() {
-    return new Promise ((resolve) => { 
+    return new Promise ((resolve, reject) => {
+        try {
         // 2.Create the XHR
-    const req = new XMLHttpRequest()
-    // 3.Add listener for when the response is recieved and parsed
-    // 9.Listener callback is triggered, which in turn calls cd, passing the joke
-    req.addEventListener("load", event => resolve(event.target.response))
-    // 4.Open the URL with the appropriate http request verb
-    req.open("Get", "https://icanhazdadjoke.com/")
-    // 5.Open Accept header so server gives us JSON
-    req.setRequestHeader("Accept", "application/json")
-    // 6.Set responseType so XHR object parses the JSON
-    req.responseType = "json"
-    // 7.Send the request, the immediately return from getJoke (i.e don't wait!)
-    req.send()
-})
-   
+        const req = new XMLHttpRequest()
+        // 3.Add listener for when the response is recieved and parsed
+        // 9.Listener callback is triggered, which in turn calls cd, passing the joke
+        req.addEventListener("load", event => resolve(event.target.response.joke))
+        // 4.Open the URL with the appropriate http request verb
+        req.open("Get", "https://icanhazdadjoke.com/")
+        // 5.Open Accept header so server gives us JSON
+        req.setRequestHeader("Accept", "application/json")
+        // 6.Set responseType so XHR object parses the JSON
+        req.responseType = "json"
+        // 7.Send the request, the immediately return from getJoke (i.e don't wait!)
+        req.send()
+        }
+        catch (e) {
+            reject(e)
+        }
+        })
 }
+
+function fetchJoke() {
+    fetch("https://icanhazdadjoke.com/", {
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+        .then ((res) => res.json())
+        .then((data) => console.log(data))
+}
+
+fetchJoke()
 
 // 1.Call getJoke and pass a callback function
 // 10.Callback is called via cb in the load listener in getJoke, recieving the joke
-getJoke().then(joke => console.log(joke))
+
+// let jokes = []
+
+// getJoke()
+//     .then(joke => {
+//         joke.push(joke)
+//         return getJoke()
+//     })
+//     .then(joke => {
+//         joke.push(joke)
+//         return getJoke()
+//     })
+//     .then(joke => {
+//         jokes.push(joke)
+//         return getJoke()
+//     })
+//     .then(joke => {
+//         jokes.push(joke)
+//         console.log(jokes)
+//     })
+
+// const jokePromises = []
+
+// for (let i = 0; i < 3; i++) {
+//     jokePromises.push(getJoke())
+// }
+
+// Promise.all(jokePromises)
+//     .then(jokes => console.log(jokes))
+//     .catch(err => console.error(err))
+
 
 
 // 8.getJoke returned immediately, so log out message
