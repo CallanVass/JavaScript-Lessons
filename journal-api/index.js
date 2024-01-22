c2cd97789c63a7c9175a0068e0a3e83406b97156
Callan <callan.v@fake.com>
2024-01-22 13:57:26 +1100

    Completed lesson on Express.js

--- journal-api/index.js
+++ journal-api/index.js
@@ -4,7 +4,7 @@ const categories = ["Food", "Gaming", "Coding", "Other"]
 
 const entries = [
     {category: "Food", content: "Pizza is yummy!"},
-    {category: "Codeing", content: "Coding is fun!"},
+    {category: "Coding", content: "Coding is fun!"},
     {category: "Gaming", content: "Skyrim is for the Nords!"}
 
 ]
@@ -13,26 +13,45 @@ const entries = [
 // Instance of application
 const app = express()
 
+app.use(express.json())
+
 // HTTP request (GET)
 app.get('/', (req, res) => {
     res.send({ info: "Journal API" })
 })
 
+// HTTP request (GET)
 app.get('/categories', (req, res) => {
     res.status(201).send(categories)
 })
 
+// HTTP request (GET)
 app.get('/entries', (req, res) => {
     res.status(201).send(entries)
 })
 
+// Get One Entry
+app.get('/entries/:id', (req, res) => {
+    const entry = entries[req.params.id - 1]
+    if (entry) {
+        res.send(entry)
+    } else {
+        res.status(404).send( {error: "Entry not found!"})
+    }
+})
+
+// HTTP request (POST)
 app.post('/entries', (req, res) => {
     // Get entry data from the request
-    // Validate
+    console.log(req.body)
+    // TODO: Validate
     // Create a new entry object
     // Push the new entry to the array
+    entries.push(req.body)
     // Respond with 201 and the created entry
+    res.status(201).send(entries[entries.length - 1])
 })
+
 
 // Start server, listen for connections on this port
 app.listen(4001)
