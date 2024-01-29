f0f1718e237437057b3ee6c374aca0368bd22ace
Callan <callan.v@fake.com>
2024-01-29 13:35:18 +1100

    Finished react lesson

--- react-intro/src/App.jsx
+++ react-intro/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
   return (
     <>
         <h1>Hello</h1>
-        <Greeting />
-        <Greeting />
+        <Greeting foo="bar" name="Callan" />
+        <Greeting abc="123" age={51} />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, libero natus laudantium non reprehenderit et quo error vitae aperiam deserunt voluptatibus. Sapiente odio nulla, tempore fuga enim voluptates doloribus reiciendis.</p>
     </>
   )
--- react-intro/src/Greeting.jsx
+++ react-intro/src/Greeting.jsx
@@ -1,9 +1,11 @@
-function Greeting() {
-    
+function Greeting({ name="None", age }) {
+    console.log()
+    // Destructure props so we don't have to use props.name
+    // const  = props
     return (
     <>
-        <p>Bonjour!</p>
-        <p>Hola!</p>
+        <p>Bonjour, {name}</p>
+        <p>Your age is: {age}</p>
     </>
     )
 }
